1d12ee0d5c1ff621e1b47255334ae964d062b385
borkymcgee <juno.presken+github@protonmail.com>
2025-06-08 14:22:46 -0700

    made button listeners programmatic

--- app/src/main/java/com/example/asciiinsanity/MainActivity.kt
+++ app/src/main/java/com/example/asciiinsanity/MainActivity.kt
@@ -24,6 +24,9 @@ class MainActivity : AppCompatActivity() {
         val button6 = findViewById<Button>(R.id.button6)
         val button7 = findViewById<Button>(R.id.button7)
 
+        //put buttons into array for programmatic assignment
+        val buttons:Array<Button> = arrayOf(button0, button1, button2, button3, button4, button5, button6, button7)
+
         val displayedAscii = findViewById<TextView>(R.id.displayedAscii)    //the character the user is trying to guess
         val binaryHint = findViewById<TextView>(R.id.binaryHint)    //the binary of displayedAscii (for easier debugging)
         val displayedCurrentGuess = findViewById<TextView>(R.id.displayedCurrentGuess)  //the character the user's current guess corresponds to
@@ -39,53 +42,17 @@ class MainActivity : AppCompatActivity() {
         //display current guess in binary for input debugging
         displayedCurrentGuess.setText(currentGuess.code.toString(2).padStart(8,'0'))
 
-
-
-        //when buttons are clicked, update currentGuess, and change their text to reflect their new value
-        //this can be done programmatically, will do this once i create a git repo
-        button0.setOnClickListener() {
-            currentGuess = (currentGuess.code xor 0b1).toChar()
-            button0.setText((currentGuess.code and 1).toString())
-            displayedCurrentGuess.setText(currentGuess.code.toString(2).padStart(8,'0'))
+        //iterate through buttons, flipping bits if the button has been pressed
+        for(i in 0..7){
+            buttons[i].setOnClickListener() {
+                //update guess with flipped bit
+                currentGuess = (currentGuess.code xor (1 shl i)).toChar()
+                //set text on button to reflect flipped bit
+                buttons[i].setText(((currentGuess.code shr i) and 1).toString())
+                //update current guess display
+                displayedCurrentGuess.setText(currentGuess.code.toString(2).padStart(8,'0'))
+            }
         }
-        button1.setOnClickListener() {
-            currentGuess = (currentGuess.code xor 0b10).toChar()
-            button1.setText(((currentGuess.code shr 1) and 1).toString())
-            displayedCurrentGuess.setText(currentGuess.code.toString(2).padStart(8,'0'))
-        }
-        button2.setOnClickListener() {
-            currentGuess = (currentGuess.code xor 0b100).toChar()
-            button2.setText(((currentGuess.code shr 2) and 1).toString())
-            displayedCurrentGuess.setText(currentGuess.code.toString(2).padStart(8,'0'))
-        }
-        button3.setOnClickListener() {
-            currentGuess = (currentGuess.code xor 0b1000).toChar()
-            button3.setText(((currentGuess.code shr 3) and 1).toString())
-            displayedCurrentGuess.setText(currentGuess.code.toString(2).padStart(8,'0'))
-        }
-        button4.setOnClickListener() {
-            currentGuess = (currentGuess.code xor 0b10000).toChar()
-            button4.setText(((currentGuess.code shr 4) and 1).toString())
-            displayedCurrentGuess.setText(currentGuess.code.toString(2).padStart(8,'0'))
-        }
-        button5.setOnClickListener() {
-            currentGuess = (currentGuess.code xor 0b100000).toChar()
-            button5.setText(((currentGuess.code shr 5) and 1).toString())
-            displayedCurrentGuess.setText(currentGuess.code.toString(2).padStart(8,'0'))
-        }
-        button6.setOnClickListener() {
-            currentGuess = (currentGuess.code xor 0b1000000).toChar()
-            button6.setText(((currentGuess.code shr 6) and 1).toString())
-            displayedCurrentGuess.setText(currentGuess.code.toString(2).padStart(8,'0'))
-        }
-        button7.setOnClickListener() {
-            currentGuess = (currentGuess.code xor 0b10000000).toChar()
-            button7.setText(((currentGuess.code shr 7) and 1).toString())
-            displayedCurrentGuess.setText(currentGuess.code.toString(2).padStart(8,'0'))
-        }
-
-
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
