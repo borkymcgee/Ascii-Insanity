d3f69023635ef21244645b04b3c1939e9e4a196c
borkymcgee <juno.presken+github@protonmail.com>
2025-06-08 22:18:16 -0700

    added difficulty and different game modes

--- app/src/main/java/com/example/asciiinsanity/MainActivity.kt
+++ app/src/main/java/com/example/asciiinsanity/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+    @OptIn(ExperimentalStdlibApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,20 +28,65 @@ class MainActivity : AppCompatActivity() {
         val button6 = findViewById<Button>(R.id.button6)
         val button7 = findViewById<Button>(R.id.button7)
 
+        val nonPrinting:Array<String> = arrayOf("NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL", "BS", "HT", "LF", "VT", "FF", "CR", "SO", "SI", "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB", "CAN", "EM", "SUB", "ESC", "FS", "GS", "RS", "US", "SP")
+
         //put buttons into array for programmatic assignment
         val buttons:Array<Button> = arrayOf(button0, button1, button2, button3, button4, button5, button6, button7)
+
+        val modes:Array<String> = arrayOf("dec", "hex", "ascii")
+        val difficulties:Array<String> = arrayOf("easy", "hard", "expert")
 
         val asciiTable = findViewById<ImageView>(R.id.asciiTable)    //the character the user is trying to guess
         val displayedAscii = findViewById<TextView>(R.id.displayedAscii)    //the character the user is trying to guess
         val binaryHint = findViewById<TextView>(R.id.binaryHint)    //the binary of displayedAscii (for easier debugging)
         val displayedCurrentGuess = findViewById<TextView>(R.id.displayedCurrentGuess)  //the character the user's current guess corresponds to
-        
-        var asciiToGuess = (33..126).random().toChar()  //random ascii character guess
+        val gameModeButton = findViewById<TextView>(R.id.gameModeButton)
+        val gameDifficultyButton = findViewById<TextView>(R.id.gameDifficultyButton)
+
+        var difficulty = 0  //0 = easy, 1 = hard, 2 = expert
+        var gameMode = 2    //0 = dec, 1 = hex, 2 = ascii
+
+        //randomly picks a target to guess depending on game mode and difficulty
+        fun randomTarget(): Char {
+            if(gameMode < 2) {  //if decimal or hex
+                when(difficulty) {
+                    0 -> return (0..15).random().toChar()   //decimal easy
+                    1 -> return (0..255).random().toChar() //decimal hard
+                }
+            } else {    //if ascii
+                when(difficulty) {
+                    0 -> return (65..90).random().toChar()  //ascii easy
+                    1 -> return (33..126).random().toChar() //ascii hard
+                    2 -> return (0..127).random().toChar()  //ascii expert
+                }
+            }
+            return 'X'
+        }
+
+        var asciiToGuess = randomTarget()  //random ascii character guess
+        //var asciiToGuess = (0..128).random().toChar()  //random ascii character guess
         var currentGuess = 0.toChar()   //set the current guess to 0
+
+
+        //returns a string representation of any ascii character,
+        fun charToAscii(c: Char): String {
+            if(c.code in 0..32) return nonPrinting[c.code]
+            else if(c.code == 127) return "DEL"
+            else return c.toString()
+        }
         
         fun updateNumbers() {
-            //update the target text
-            displayedAscii.setText(asciiToGuess.toString())
+            //update the target text, depending on gameMode
+            when(gameMode) {
+                0 -> displayedAscii.setText(asciiToGuess.code.toString())
+                1 -> {
+                    if(difficulty == 0) displayedAscii.setText(asciiToGuess.code.toInt().toHexString().substring(7))
+                    else displayedAscii.setText(asciiToGuess.code.toInt().toHexString().substring(6))
+                }    //truncate bc tohexstring adds leading 0s
+                2 -> displayedAscii.setText(charToAscii(asciiToGuess))
+            }
+            gameDifficultyButton.setText(difficulties[difficulty])
+            gameModeButton.setText(modes[gameMode])
             //update the hint text
             binaryHint.setText(asciiToGuess.code.toString(2).padStart(8,'0'))
             //update the current guessed character
@@ -50,6 +96,36 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateNumbers()
+
+        //cycle through modes on click
+        gameModeButton.setOnClickListener(){
+            if(gameMode >= 2) gameMode = 0
+            else gameMode++
+
+            if(difficulty == 2) difficulty = 1
+
+            updateNumbers()
+        }
+
+        //toggle difficulty on click
+        gameDifficultyButton.setOnClickListener(){
+            if(difficulty >= 1){
+                difficulty = 0
+                var asciiToGuess = randomTarget()  //random ascii character guess
+            } else {
+                difficulty++
+            }
+
+            updateNumbers()
+        }
+
+        gameDifficultyButton.setOnLongClickListener() {
+            difficulty = 2
+            gameMode = 2
+
+            updateNumbers()
+            return@setOnLongClickListener true
+        }
 
         //toggle visibility of ascii table when current guess is tapped
         displayedCurrentGuess.setOnClickListener(){
@@ -71,7 +147,7 @@ class MainActivity : AppCompatActivity() {
                     //congratulate user
                     Toast.makeText(getApplicationContext(), "You did it!", Toast.LENGTH_LONG).show()
                     //reset target
-                    asciiToGuess = (33..126).random().toChar()
+                    asciiToGuess = randomTarget()
                     //reset guess
                     currentGuess = 0.toChar()
                     
